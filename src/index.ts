25ca60b1f5f4fb513fd0736364a5598c8b59a26a
vincent <vincent.faye@koparexpress.com>
2021-11-24 16:47:53 +0000

    adding create request action

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+import axios from "axios";
+dotenv.config()
+
 const printingKoparPayInConsole = () => {
   return `
     ###    ###  ##########  #########   #########  ########        #########    ##########  ###    ###
@@ -17,13 +21,36 @@ console.log(printingKoparPayInConsole());
 // Starting
 
 export interface IKoparPayOptions {
-  apiKey,
-  apiUrl
+  apiKey
 }
 
-export interface IKoparPayRequest {}
+export interface IKoparPayRequest {
+  apiKey: string,
+  itemPrice: number,
+  commandRef: string,
+  commandName: string,
+  ipnUrl: string,
+  successUrl: string,
+  cancelUrl: string,
+  firstName: string,
+  lastName: string,
+  phoneNumber: string,
+  email: string,
+  currency: string
+}
 
-export interface IKoparPayResponse {}
+export interface IKoparPayResponse {
+  message,
+  status,
+  token
+}
+
+const headers = {
+  'content-type': 'application/json',
+  'accept': 'application/json'
+}
+
+const API_URL = process.env.API_URL
 
 export class KoparPay {
   private readonly options: IKoparPayOptions;
@@ -32,7 +59,18 @@ export class KoparPay {
     this.options = {...options}
   }
 
-  displayKoparPayInfos(): string {
-    return `displaying kopar pay credentials : API_KEY = ${this.options['apiKey']} and API_URL = ${this.options['apiUrl']}`;
+  getApiUrl(): string {return `${API_URL}/transaction/request`}
+
+  createRequestPayment(payload: IKoparPayRequest): Promise<IKoparPayResponse> {
+    return new Promise((resolve, reject) => {
+      axios({
+        url: this.getApiUrl(),
+        method: 'POST',
+        data: payload,
+        headers
+      })
+        .then(response => resolve(response.data))
+        .catch(error => reject(error))
+    })
   }
 }
